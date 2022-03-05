47e63c71780aa45e4e6d0469ee9306ce07229ffb
tyatyaedo <48467786+tyatyaedo@users.noreply.github.com>
2022-03-05 15:28:27 +0900

    add screen shot.

--- TestTool/__main__.py
+++ TestTool/__main__.py
@@ -2,9 +2,37 @@
 # -*- coding: utf8 -*-
 import sys
 import tkinter as tk
- 
+from PIL import ImageGrab
+from pynput import keyboard
+
+def screen_shot():
+    # フルスクリーンで取得する
+    screenshot = ImageGrab.grab()
+    screenshot.save('screenshot.jpg')
+
+def press(key):
+    try:
+        if key == keyboard.Key.print_screen:
+            print('アルファベット {0} が押されました'.format(key.char))
+    except AttributeError:
+        if key == keyboard.Key.print_screen:
+            screen_shot()
+
+def release(key):
+    if key == keyboard.Key.esc:     # escが押された場合
+        return False    # listenerを止める
+
+listener = keyboard.Listener(
+    on_press=press,
+    on_release=release)
+listener.start()
+
 root = tk.Tk()
-root.title(u"Software Title")
+root.title(u"Test Tools")
 root.geometry("400x300")
+
+# Ref Button
+buttonRef = tk.Button(root, text = '参照')
+buttonRef.grid(column=0, row=0, padx=5, pady=5, sticky=tk.W+tk.E+tk.N+tk.S)
 
 root.mainloop()
